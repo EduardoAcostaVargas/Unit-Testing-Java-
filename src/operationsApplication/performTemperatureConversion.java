9521d681aec5bebe0c157390c9ed067c06dd9955
eduardo <ejav97@outlook.com>
2026-03-17 11:17:38 -0500

    final commit.

--- src/operationsApplication/performTemperatureConversion.java
+++ src/operationsApplication/performTemperatureConversion.java
@@ -12,7 +12,7 @@ public class performTemperatureConversion {
         System.out.println("---------- TEMPERATURE CONVERTER ----------");
         System.out.println("1. Celsius to Fahrenheit");
         System.out.println("2. Fahrenheit to Celsius");
-        System.out.println("Select an option (1 or 2: ");
+        System.out.println("Select an option (1 or 2): ");
         int option = scanner.nextInt();
 
         switch (option){
@@ -20,14 +20,20 @@ public class performTemperatureConversion {
                 System.out.println("Enter temperature in Celsius");
                 double celsius = scanner.nextDouble();
                 double fahrenheit = temperatureConverter.celsiusToFahrenheit(celsius);
-                System.out.println(celsius + " Celsius is " + " " + fahrenheit + " in Fahrenheit.");
+
+                String roundedFahrenheit = String.format("%.2f", fahrenheit);
+
+                System.out.println(celsius + " Celsius is " + " " + roundedFahrenheit + " in Fahrenheit.");
                 break;
             }
             case 2 : {
                 System.out.println("Enter temperature in Fahrenheit");
                 double fahrenheit = scanner.nextDouble();
                 double celsius = temperatureConverter.fahrenheitToCelsius(fahrenheit);
-                System.out.println(fahrenheit + " Fahrenheit is " + " " + celsius + " in Fahrenheit.");
+
+                String roundedCelsius = String.format("%.2f", celsius);
+
+                System.out.println(fahrenheit + " Fahrenheit is " + " " + roundedCelsius + " in Fahrenheit.");
                 break;
             }
             default:{
